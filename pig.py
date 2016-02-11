8a7cdd39081009718fc95e18b20fd01a34261d16
Alexander Green <aeg0322@gmail.com>
2016-02-11 06:48:58 -0500

    save

---
+++ pig.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python 3
+import skilstak.colors as c
+import time
+
+if __name__ == "__main__":
+    print("What word would you like to translate into pig latin?")
+    word = input(">>> ")
+
