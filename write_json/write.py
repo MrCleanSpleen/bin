644ee833f0e135e84fc24fa59ac65cb8dbfefecb
Alexander Green <aeg0322@gmail.com>
2016-03-11 11:22:32 -0500

    save

--- write_json/write.py
+++ write_json/write.py
@@ -4,44 +4,29 @@ import skilstak.colors as c
 import json
 def write_json():
     name = input(c.clear + "What is the name of your class? >>> ")
-    health = input(c.clear + "What is your class's helath? >>> ")
-    agility = input(c.clear + "What is your class's agility? >>> ")
-    shield = input(c.clear + "What is your class's defense? >>> ")
+    health = float(input(c.clear + "What is your class's helath? >>> "))
+    agility = float(input(c.clear + "What is your class's agility? >>> "))
+    shield =float( input(c.clear + "What is your class's defense? >>> "))
     move1 = input(c.clear + "What is the name of your first move? >>> ")
     damage1 = float(input(c.clear + "What is the damage of the move? >>> "))
     accuracy1 = float(input(c.clear + "What is the accuracy of the move? >>> "))
-    speed1 = float(input(c.clear + "What is the speed of the move? >>> ")
+    speed1 = float(input(c.clear + "What is the speed of the move? >>> "))
     move2 = input(c.clear + "What is the name of your second move? >>> ")
     damage2 = float(input(c.clear + "What is the damage of the move? >>> "))
     accuracy2 = float(input(c.clear + "What is the accuracy of the move? >>> "))
-    speed2 = float(input(c.clear + "What is the speed of the move? >>> ")
-    data = """
-    { 
-        "class":"""" + name + """",
-        "agility":"""" + agility + """",
-        "shield":"""" + shield + """",
-        "health":"""" + health + """",
-        "attacks": [
-
-       {
-        "name":"""" + move1 + """",
-        "damage":"""" + damage1 + """",
-        "accuracy":"""" + accuracy1 + """",
-        "speed":"""" + speed1 + """"
-
-        },
-        {
-        "name":"""" + move2 + """",
-        "damage":"""" + damage2 + """",
-        "accuracy":"""" + accuracy2 + """",
-        "speed":"""" + speed2 + """"
-        }]
-    }
-    """
+    speed2 = float(input(c.clear + "What is the speed of the move? >>> "))
+    data = {
+        "class":name,
+        "health":health,
+        "agility":agility,
+        "shield":shield
+            
+        }
+    
     json = name + ".json"
-    with open(json, "r+") as f:
-    pass
-    out_file =  open(json,"w")    
+    with open(json, "w+") as f:
+        pass
+    out_file = open(json,"w")    
     json.dump(data,out_file, indent=4)
 
     
